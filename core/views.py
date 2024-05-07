c011258ba869998b5fb10c58e47b01cca2e19b03
Zly Wik <wiktor.dlugi.bader@gmail.com>
2024-05-07 12:14:07 +0200

    CRUD for Blog & Posts, User and UserProfile models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Blog, Post
+from core.serializers import BlogSerializer, PostSerializer
 
 # Create your views here.
+class BlogViewSet(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+
+class PostViewSet(ModelViewSet):
+    # queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    def get_queryset(self):
+        return Post.objects.filter(blog=self.kwargs['blog_pk'])
+
+    def get_serializer_context(self):
+        return {'blog': self.kwargs['blog_pk']}
