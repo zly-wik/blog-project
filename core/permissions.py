f5a2bfb5a03eefb6421c62a75df93a982655b6db
Zly Wik <wiktor.dlugi.bader@gmail.com>
2024-05-12 18:04:58 +0200

    BlogOwnerOrReadOnly permission added to Blog- and PostViewSet

---
+++ core/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+
+class IsBlogOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owner of an blog to edit it.
+    Assumes the model instance has an `blog` or 'owner attribute.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if hasattr(obj, 'owner'):
+            print(obj.owner.user == request.user)
+            return obj.owner.user == request.user
+        
+        if hasattr(obj, 'blog'):
+            return obj.blog.owner.user == request.user
